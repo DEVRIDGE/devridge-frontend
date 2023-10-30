3b67245e64828e3858c1972cd7d38117c70dcdd1
배민수 <duecost@gmail.com>
2023-10-30 22:36:40 +0900

    구글 애널리틱스 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "react-query";
+import ReactGA from "react-ga4";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+
+if (process.env.NODE_ENV === "production") {
+  ReactGA.initialize("G-LJFNNS7K3Y");
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -16,7 +22,13 @@ const queryClient = new QueryClient();
 root.render(
   <RecoilRoot>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter
+        basename={
+          process.env.NODE_ENV === "development" ? process.env.PUBLIC_URL : "/"
+        }
+      >
+        <App />
+      </BrowserRouter>
     </QueryClientProvider>
   </RecoilRoot>
 );
